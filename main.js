e2232111b81b3b020fa3b77a800efcecaeabfcbf
Vinicius Costa de Oliveira <79426395+Vinnh@users.noreply.github.com>
2022-09-12 14:56:14 -0300

    novas alterações + responsividade

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+})
